23f07a0810caa58acab0d2e13af8a909fbfcf846
ilya <mizonov.iv@gmail.com>
2026-05-18 12:42:40 +0500

    initial commit: installed - nuxt, tailwind, pinia

---
+++ app/pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container-page">
+    <h1 class="text-4xl font-bold">
+      Главная страница
+    </h1>
+  </div>
+</template>
+
+<script setup>
+useSeoMeta({
+  title: 'Главная',
+  description: 'Главная страница',
+})
+</script>
---
+++ nuxt.config.ts
@@ -0,0 +1,40 @@
+export default defineNuxtConfig({
+    devtools: {
+        enabled: true,
+    },
+
+    modules: [
+        '@nuxtjs/tailwindcss',
+        '@pinia/nuxt',
+    ],
+
+    css: [
+        '~/assets/css/main.css',
+    ],
+
+    runtimeConfig: {
+        public: {
+            // apiUrl: process.env.API_URL,
+        },
+    },
+
+    app: {
+        head: {
+            htmlAttrs: {
+                lang: 'ru',
+            },
+
+            meta: [
+                {
+                    charset: 'utf-8',
+                },
+                {
+                    name: 'viewport',
+                    content: 'width=device-width, initial-scale=1',
+                },
+            ],
+        },
+    },
+
+    compatibilityDate: '2026-05-18',
+})
