86c55e9ec91af028edfdb464cda9e994609dd01f
ilya <mizonov.iv@gmail.com>
2026-06-29 22:18:07 +0500

    feat: ssr disabled

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@ export default defineNuxtConfig({
     devtools: {
         enabled: process.env.NODE_ENV === 'development'
     },
-    ssr: true,
+    ssr: false,
     nitro: {
         compressPublicAssets: true
     },
